43a209b29386a8c64990a514dad38f517932326b
kudrom <kudrom@riseup.net>
2014-10-25 16:15:39 +0200

    it seems to work

--- export.py
+++ export.py
@@ -1,6 +1,10 @@
 import json
 from pymongo import MongoClient
 import sys
+import pprint
+
+def find_closest(controls, experiment):
+    return min(controls, key=lambda x: abs(x['start_time'] - experiment['start_time']))
 
 def main(hashes_filename):
     # XXX: Catch exceptions
@@ -17,7 +21,7 @@ def main(hashes_filename):
 
     # Find control and experiment reports
     controls = []
-    experiments = []
+    experiments = {}
     report_ids = {}
 
     for measurement in measurements['result']:
@@ -28,17 +32,28 @@ def main(hashes_filename):
     # For each report, find if its a control or experiment.
     for report_id, measurements in report_ids.items():
         report = db.reports.find_one({"_id": report_id})
-        if report['probe_cc'] == 'NL':
+        country = report['probe_cc']
+        if country == 'NL':
             controls.extend(measurements)
         else:
-            experiments.extend(measurements)
+            if country not in experiments:
+                experiments[country] = []
+            experiments[country].extend(measurements)
 
-    for experiment in experiments:
-        closest_control = find_closest(controls, experiment)
-        status = truth_table(experiment, closest_control)
-        experiment['status'] = status
-        bridge = measurement['input']
-        output[country][bridge].append(experiment)
+    output = {}
+    for country, measurements in experiments.items():
+        for measurement in measurements:
+            closest_control = find_closest(controls, measurement)
+            print "===================="
+            print "Experiment:", measurement
+            print "Control:", closest_control
+            print "===================="
+            """
+            status = truth_table(experiment, closest_control)
+            experiment['status'] = status
+            bridge = measurement['input']
+            output[country][bridge].append(experiment)
+            """
 
 if __name__ == "__main__":
     hashes_filename = sys.argv[1]
