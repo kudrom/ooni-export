afa2658d425ddc4d920dbc1e51ae701b6bae5c25
kudrom <kudrom@riseup.net>
2014-10-25 17:11:16 +0200

    some cleanup

--- export.py
+++ export.py
@@ -20,14 +20,14 @@ def truth_table(experiment, control):
     elif result_experiment == False and result_control == False:
         return "offline"
 
-    assert(0)
-
-
-def main(hashes_filename):
-    # XXX: Catch exceptions
-    # Read bridge hashes from file
+def get_hashes(hashes_filename):
+    """ Get hashes from filename input"""
     hashes = open(hashes_filename).readlines()
     hashes = [h.rstrip() for h in hashes]
+    return hashes
+
+def main(hashes_filename):
+    hashes = get_hashes(hashes_filename)
 
     # Connect to database
     client = MongoClient('127.0.0.1', 27017)
@@ -36,17 +36,17 @@ def main(hashes_filename):
     # Find measurements we are interested in. 
     measurements = db.measurements.aggregate([{"$match": {"input": {"$in": hashes}}}])
 
-    # Find control and experiment reports
-    controls = []
-    experiments = {}
-    report_ids = {}
 
+    # Populate an auxiliary variable with the measurements of a report
+    report_ids = {}
     for measurement in measurements['result']:
         if not measurement['report_id'] in report_ids:
             report_ids[measurement['report_id']] = []
         report_ids[measurement['report_id']].append(measurement)
 
     # For each report, find if its a control or experiment.
+    controls = []
+    experiments = {}
     for report_id, measurements in report_ids.items():
         report = db.reports.find_one({"_id": report_id})
         country = report['probe_cc']
@@ -57,19 +57,19 @@ def main(hashes_filename):
                 experiments[country] = []
             experiments[country].extend(measurements)
 
+    # Generate the output
     output = {}
     for country, measurements in experiments.items():
+        if country not in output:
+            output[country] = {}
         for measurement in measurements:
             closest_control = find_closest(controls, measurement)
             status = truth_table(measurement, closest_control)
             measurement['status'] = status
             bridge = measurement['input']
-            if country not in output:
-                output[country] = {}
-            else:
-                if bridge not in output[country]:
-                    output[country][bridge] = []
-                output[country][bridge].append(measurement)
+            if bridge not in output[country]:
+                output[country][bridge] = []
+            output[country][bridge].append(measurement)
     return output
 
 if __name__ == "__main__":
