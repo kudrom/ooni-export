eb3e32446ff68b27c4968703f963e9d8bc7ec247
kudrom <kudrom@riseup.net>
2014-10-25 15:06:58 +0200

    Reports splitted

--- export.py
+++ export.py
@@ -2,24 +2,40 @@ import json
 from pymongo import MongoClient
 import sys
 
+def closest(
+
 def main(hashes_filename):
-    hashes = open(hashes_filename).readlines()
     # XXX: Catch exceptions
+    # Read bridge hashes from file
+    hashes = open(hashes_filename).readlines()
     hashes = [h.rstrip() for h in hashes]
+    
+    # Connect to database
     client = MongoClient('127.0.0.1', 27017)
     db = client.ooni
 
-    # Populate the reports with the reports we're interested in
-    reports = []
-    for h in hashes:
-        measurements = db.measurements.find({"input": h})
-        for m in measurements:
-            report = db.reports.find({"_id": m['report_id']})
-            for r in report:
-                reports.append(r)
+    # Find measurements we are interested in. 
+    measurements = db.measurements.aggregate([{"$match": {"input": {"$in": hashes}}}])
 
-    # Find control reports
+    # Find control and experiment reports
+    control = []
+    experiments = []
+    report_ids = set()
 
+    for measurement in measurements['result']:
+        report_ids.add(measurement['report_id'])
+
+        # For each report, find if its a control or experiment.
+        for report_id in report_ids:
+            report = db.reports.find_one({"_id": report_id})
+            if report['probe_cc'] == 'NL':
+                control.append([report)
+            else:
+                experiments.append(report)
+
+    # Now find which measurements are control and which are
+    # experiments.
+    for experiment in experiments:
 
 
 if __name__ == "__main__":
